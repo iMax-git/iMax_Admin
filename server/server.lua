140a6f0389b615bf3fcf2744d94fe3780b74ddc9
iMax <45369969+iMax-git@users.noreply.github.com>
2020-11-03 19:09:03 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,35 +1,85 @@
--- server
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-
-RegisterCommand("setskin", function (source, args, rawCommand)
+RegisterCommand("setskin", function (source, args)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer ~= nil then
-        local PlayerGroup = xPlayer.getGroup()
-        local SuspectPlayer = ESX.GetPlayerFromId(args[0])
-        if PlayerGroup ~= nil and (PlayerGroup == 'owner' or PlayerGroup == 'superadmin' or PlayerGroup == 'admin') then
-            ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
-                local model = GetHashKey(args[0])
-                    RequestModel(model)
-                    while not HasModelLoaded(model) do
-                        RequestModel(model)
-                        Citizen.Wait(0)
-                    end
-                    SetPlayerModel(PlayerId(), model)
-                    SetModelAsNoLongerNeeded(model)
-                    SetPedArmour(GetPlayerPed(-1), 1000000000) -- Ajout armure
-                    GetPedArmour(GetPlayerPed(-1), 1000000000)
-                    TriggerEvent('skinchanger:loadSkin', skin)
-                    TriggerEvent('esx:restoreLoadout')
-      
-              end)
+    PlayerGroup = xPlayer.getGroup()
+    local model = args[1]
+    local count = string.len(model)
+    if PlayerGroup == 'owner' or PlayerGroup == 'superadmin' or PlayerGroup == 'admin' then
+        if model == "police" then
+            local newskin = "s_m_y_cop_01"
+            TriggerClientEvent("changeskin", source, newskin)
+        elseif model == "policef" then
+            local newskin = "GCPD_Female"
+            TriggerClientEvent("changeskin", source, newskin)
+        elseif model == "medic" then
+            local newskin = "s_m_m_paramedic_01"
+            TriggerClientEvent("changeskin", source, newskin)
+        elseif model == "swat" then
+            local newskin = "s_m_y_swat_01"
+            TriggerClientEvent("changeskin", source, newskin)
+        elseif model == "swat2" then
+            local newskin = "s_m_y_swat_02"
+            TriggerClientEvent("changeskin", source, newskin)
+        elseif model == "reset" then
+            TriggerClientEvent("resetskin", source)
+        elseif count < 6 then
+            TriggerClientEvent('esx:showNotification', source, "~r~Error: ~o~Argument Invalide")
         else
-            TriggerEvent("chat:addMessage", {
-                color = {255, 0, 0},
-                multiline = true,
-                args = {"Tu n'a pas le droit d'exectuer cette commande !" }
-            })
-        end  
-    end      
-end, true)
+            TriggerClientEvent("changeskin", source, model)
+        end
+        if model == "reset" then
+            TriggerClientEvent('esx:showNotification', source, "~g~Tu à récuperer ton skin de base !")
+        elseif count > 6 then
+            TriggerClientEvent('esx:showNotification', source, "~g~Le Skin "..model.." est chargé !")
+        end
+    else
+        TriggerClientEvent('esx:showNotification', source, "~r~Tu n'a pas le droit à cette commande !")
+    end 
+end)
+
+RegisterCommand("weapequip", function (source, args)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    PlayerGroup = xPlayer.getGroup()
+    local state = args[1]
+    if state ~= "r" then
+        local state = "a"
+    end    
+    local attach = args[2]
+    if PlayerGroup == 'owner' or PlayerGroup == 'superadmin' or PlayerGroup == 'admin' then
+        if attach == "1" then
+            if state == "r" then
+                TriggerClientEvent('removeattachement', source)
+            else
+                TriggerClientEvent('addattachement', source, 'suppressor')
+            end
+        elseif attach == "2" then
+            if state == "remove" then
+                TriggerClientEvent('removeattachement', source)
+            else
+                TriggerClientEvent('addattachement', source, 'flashlight')
+            end
+        elseif attach == "3" then
+            if state == "remove" then
+                TriggerClientEvent('removeattachement', source)
+            else
+                TriggerClientEvent('addattachement', source, 'grip')
+            end
+        end
+    else
+        TriggerClientEvent('esx:showNotification', source, "~r~Tu n'a pas le droit à cette commande !")
+    end 
+end)
+
+
+RegisterCommand("carryweapon", function (source, args)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    PlayerGroup = xPlayer.getGroup()
+    local state = args[1]
+    if state == "off" then
+        TriggerClientEvent('removecarryweapon', source)
+    elseif state == "on" then
+        TriggerClientEvent('carryweapon', source)
+    end
+end)
